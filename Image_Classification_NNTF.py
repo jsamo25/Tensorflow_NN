517873f47dacc14eefa9761a7d97a206b2400d4c
Jorge <jsamo25@gmail.com>
2020-04-17 23:26:30 +0200

    Building the model

--- Image_Classification_NNTF.py
+++ Image_Classification_NNTF.py
@@ -23,12 +23,28 @@ train_images  = train_images / 255.0
 test_images = test_images / 255.0
 
 #Print 25 elements from the Training set
-plt.figure(figsize =(10,10))
-for i in range (25):
-    plt.subplot(5,5,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(train_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[train_labels[i]])
-plt.show()
+# plt.figure(figsize =(10,10))
+# for i in range (25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(train_images[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[train_labels[i]])
+# plt.show()
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation=tf.nn.relu),
+    keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer="adam",
+              loss="sparse_categorical_crossentropy",
+              metrics = ["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+# test_loss, test_acc = model.evaluate(test_images, test_labels)
+# print(f"Test Accuracy: {test_acc}")
+
